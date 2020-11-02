30e1fc9753d47f81fd0bd3ab855106e4163c1ba0
Lukasz Kwiatkowski <Kwiatkovsky1999@wp.pl>
2020-11-02 23:14:03 +0100

    add style by js

--- main.js
+++ main.js
@@ -3,3 +3,6 @@ const age = 25;
 
 console.log (`Mam na imie ${name} i mam ${age} lat`)
 
+const articleHeading = document.querySelector (`.article--heading--js`);
+
+articleHeading.innerHTML = `jestem kim?`;
