a1d4d3835e0ad4bd73ed99940cd0757f70043c94
Lukasz Kwiatkowski <Kwiatkovsky1999@wp.pl>
2020-11-04 22:32:44 +0100

    add arrow function

--- main.js
+++ main.js
@@ -6,3 +6,26 @@ console.log (`Mam na imie ${name} i mam ${age} lat`)
 const articleHeading = document.querySelector (`.article--heading--js`);
 
 articleHeading.innerHTML = `jestem kim?`;
+
+
+function calculation(myNuber) {
+   console.log (`wychodzi ${myNuber}`)
+   return myNuber*7;
+ 
+};
+myBubmer = calculation(2);
+
+console.log(myBubmer);
+
+
+function wite(name, age) {
+   console.log (`Mam na imie ${name} i mam ${age} lat`)
+
+};
+wite(name, age);
+
+const witear = (name, age) => console.log (`Mam na imie ${name} i mam ${age} lat`);
+
+witear(name, age);
+
+witear(name, age);
