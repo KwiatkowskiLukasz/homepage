a0dfd53cf96cc34be49c52442fc5c0876cb5d466
Lukasz Kwiatkowski <Kwiatkovsky1999@wp.pl>
2020-11-21 18:14:52 +0100

    correct hamburger and style

--- main.js
+++ main.js
@@ -37,5 +37,5 @@ const button = document.querySelector(`.hamburger--js`);
 
 button.addEventListener(`click`, () => {
    const nav = document.querySelector(`.navigation--js`);
-   nav.classList.toggle(`navigation--open`);
+   nav.classList.toggle(`navigation--link--open`);
 })
